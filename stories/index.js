8b570eededc355dc0df2691a51777a532ba59070
Camilo Gomez <camilogomezmusic@gmail.com>
2020-09-01 13:39:20 -0400

    Form component and stories

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show"
 import Confirm from "components/Appointment/Confirm"
 import Status from "components/Appointment/Status"
 import Error from "components/Appointment/Error"
+import Form from "components/Appointment/Form"
 
 
 storiesOf("Button", module)
@@ -152,6 +153,8 @@ storiesOf("InterviewerList", module)
     .add("Deleting Status", () => <Status message="Deleting" />)
     .add("Saving Status", () => <Status message="Saving" />)
     .add("Error", () => <Error message="Could not delete appointment" onClose={action("onClose")} />)
+    .add("Create", () => <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />)
+    .add("Edit", () => <Form name="Camilo Gomez" interviewers={interviewers} interviewer={3} onSave={action("onSave")} onCancel={action("onCancel")} />)
 
 
       
