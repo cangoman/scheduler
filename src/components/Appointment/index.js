bd1d7b8f13610dd5789e058cb4f6b33d971ec61e
Camilo Gomez <camilogomezmusic@gmail.com>
2020-09-03 12:20:36 -0400

    Added error mode transitions

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,18 +6,22 @@ import Show from "components/Appointment/Show";
 import Form from "components/Appointment/Form";
 import Status from "components/Appointment/Status";
 import Confirm from "components/Appointment/Confirm";
+import Error from "components/Appointment/Error";
 
 import useVisualMode from "hooks/useVisualMode";
 
 import "components/Appointment/styles.scss"
 
-const EMPTY     = "EMPTY";
-const SHOW      = "SHOW";
-const CREATE    = "CREATE";
-const EDIT      = "EDIT";
-const SAVING    = "SAVING";
-const DELETING  = "DELETING";
-const CONFIRM   = "CONFIRM";
+const EMPTY         = "EMPTY";
+const SHOW          = "SHOW";
+const CREATE        = "CREATE";
+const EDIT          = "EDIT";
+const SAVING        = "SAVING";
+const DELETING      = "DELETING";
+const CONFIRM       = "CONFIRM";
+const ERROR_SAVE    = "ERROR_SAVE";
+const ERROR_DELETE  = "ERROR_DELETE ";
+
 
 
 export default function Appointment(props) {
@@ -30,12 +34,18 @@ export default function Appointment(props) {
       interviewer
     }
     transition(SAVING);
-    props.bookInterview(props.id, interview).then( () => transition(SHOW));
+    props
+      .bookInterview(props.id, interview)
+      .then( () => transition(SHOW))
+      .catch( () => transition(ERROR_SAVE, true));
   }
 
   const deleteAppointment = () => {
-    transition(DELETING);
-    props.cancelInterview(props.id).then( () => transition(EMPTY))
+    transition(DELETING, true);
+    props
+      .cancelInterview(props.id)
+      .then( () => transition(EMPTY))
+      .catch( () => transition(ERROR_DELETE, true))
   }
 
   
@@ -92,6 +102,18 @@ export default function Appointment(props) {
           onCancel={back}
         />
         )
+      }
+      {mode === ERROR_DELETE &&
+        (<Error 
+          message="Could not cancel appointment" 
+          onClose={back} 
+        />)
+      }
+      {mode === ERROR_SAVE &&
+        (<Error 
+          message="Could not save appointment" 
+          onClose={back} 
+        />)
       }
 
 
